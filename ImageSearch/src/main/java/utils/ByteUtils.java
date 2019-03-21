36a1b23beb91464d3cbd25773c62bc9f728ea6ed
Honghao Wang <hongwang@ebay.com>
2019-03-20 20:46:20 -0400

    add ByteUtils

--- ImageSearch/src/main/java/utils/ByteUtils.java
+++ ImageSearch/src/main/java/utils/ByteUtils.java
@@ -1,5 +1,41 @@
 package utils;
 
-public class ByteUtils {
+import java.nio.ByteBuffer;
 
+public class ByteUtils {
+    private static ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static byte[] longToBytes(long x) {
+        buffer.putLong(0, x);
+        return buffer.array();
+    }
+
+    public static long bytesToLong(byte[] bytes) {
+        buffer.put(bytes, 0, bytes.length);
+        buffer.flip();//need flip 
+        return buffer.getLong();
+    }
+    
+    public static long bytesToLong(byte[] bytes, int offset) {
+    	buffer.put(bytes, offset, Long.BYTES);
+        buffer.flip();//need flip 
+        return buffer.getLong();
+    }
+        
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+    
+    public static void main(String[] args) {
+    	byte[] arr = longToBytes(15);
+    	System.out.println(bytesToHex(arr));
+    	
+    }
 }
